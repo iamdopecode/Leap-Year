c64949251620be36219524ea3739516d7194cece
Shubham <shubham13202@yahoo.com>
2019-05-22 13:29:21 +0530

    Initial Commit

---
+++ Leap Year.py
@@ -0,0 +1,14 @@
+"""
+A leap year is a year which is divisible by 4 & 400 and gives remainder as 0.And which is not eqauls to 100.
+So in this program we will first import a module and,then calls one of its function
+"""
+
+
+import calendar                          # Imported a module named calender
+n=int(input("enter the year :"))         # Takes value from the user in integer format
+print(calendar.isleap(2018))             # Finally call it functions 
+
+
+
+
+# Code by Shubham Sharma , Freelancer Developer
---
+++ README.md
@@ -0,0 +1,20 @@
+# Leap Year program in Python 3.7
+Leap Year is years which are divide by 4 and 400 and gives remainder as 0 , And which is  not equals to 100 .
+
+And this is simplest code of Leap Year in Python
+
+___
+___
+
+# Prerequisite
+
+1.Python Idle 3.7
+
+---
+
+
+Thank You
+
+---
+---
+
